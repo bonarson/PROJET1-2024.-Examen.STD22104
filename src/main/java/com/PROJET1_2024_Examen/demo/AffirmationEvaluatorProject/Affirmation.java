7f89e33b5096369224455a08aadbcfdb0ac7772b
bonarson <hei.bonarson@gmail.com>
2024-06-07 15:27:57 +0300

    feat:Add sealed abstract class Affirmation with permits for SimpleAffirmation and CompositeAffirmation

--- src/main/java/com/PROJET1_2024_Examen/demo/AffirmationEvaluatorProject/Affirmation.java
+++ src/main/java/com/PROJET1_2024_Examen/demo/AffirmationEvaluatorProject/Affirmation.java
@@ -1,5 +1,5 @@
 package com.PROJET1_2024_Examen.demo.AffirmationEvaluatorProject;
 
-public class Affirmation {
-
+public sealed abstract class Affirmation permits SimpleAffirmation, CompositeAffirmation {
+    abstract boolean evaluate();
 }
