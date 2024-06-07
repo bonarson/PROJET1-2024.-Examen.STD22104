d499811c1871262715182c6f30a44b5b9afcc9f4
bonarson <hei.bonarson@gmail.com>
2024-06-07 15:43:14 +0300

    feat:Add setup method in AffirmationEvaluatorTest to initialize AffirmationEvaluator and add basic affirmations

--- src/test/java/com/PROJET1_2024_Examen/demo/AffirmationEvaluatorProject/AffirmationEvaluatorTest.java
+++ src/test/java/com/PROJET1_2024_Examen/demo/AffirmationEvaluatorProject/AffirmationEvaluatorTest.java
@@ -1,5 +1,17 @@
 package com.PROJET1_2024_Examen.demo.AffirmationEvaluatorProject;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class AffirmationEvaluatorTest {
+    private AffirmationEvaluator evaluator;
+
+    @BeforeEach
+    void setUp() {
+        evaluator = new AffirmationEvaluator();
+
+        evaluator.addAffirmation("vérité_1", new SimpleAffirmation(true));
+        evaluator.addAffirmation("mensonge_1", new SimpleAffirmation(false));
+        evaluator.addAffirmation("affirmation_1", new SimpleAffirmation(false));
+    }
 
 }
